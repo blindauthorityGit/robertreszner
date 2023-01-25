4b5ed1730a557d97b253ee64772c9b4037da1875
Johannes <office@atelierbuchner.at>
2023-01-25 12:00:17 +0100

    fix

--- pages/works/[slug].jsx
+++ pages/works/[slug].jsx
@@ -353,6 +353,6 @@ export const getStaticProps = async (context) => {
             post: data[0],
             dataAll,
         },
-        revalidate: 1, // 10 secondss
+        revalidate: 1,
     };
 };
