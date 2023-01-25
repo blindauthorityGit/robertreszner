8b4e8a47249f144c31cb7fe6f678d885a78a9d5c
Johannes <office@atelierbuchner.at>
2023-01-25 12:35:05 +0100

    fix

--- client.js
+++ client.js
@@ -5,5 +5,6 @@ export default sanityClient({
     projectId: "bsxaqchg", // you can find this in sanity.json
     dataset: "production", // or the name you chose in step 1
     useCdn: true, // `false` if you want to ensure fresh data
+    apiVersion: "2023-01-25",
 });
 export { getAsset };
